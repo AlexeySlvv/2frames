65ed521241c4cf718e0fc3e98633c5044b7187dd
AlexeySlvv <me@alexeyslvv.ru>
2022-08-25 15:32:00 +0300

    add icon

--- 2frames.py
+++ 2frames.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 
 from main_window import MainWindow
 
@@ -7,6 +8,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     window = MainWindow()
+    window.setWindowIcon(QIcon('icon.ico'))
     window.setMinimumSize(640, 320)
     window.show()
 
